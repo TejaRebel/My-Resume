28fda3487c1f0d010955e2331654f249ad4c2658
Teja Koribilli <113105884+TejaRebel@users.noreply.github.com>
2023-03-22 13:08:05 +0530

    Add files via upload

---
+++ Resume/app.js
@@ -0,0 +1,11 @@
+function openMenu() {
+  document.querySelector(".menu").style.display = "none";
+  document.querySelector(".navbar").style.display = "block";
+  document.querySelector(".close").style.display = "block";
+}
+
+function closeMenu() {
+  document.querySelector(".menu").style.display = "block";
+  document.querySelector(".navbar").style.display = "none";
+  document.querySelector(".close").style.display = "none";
+}
